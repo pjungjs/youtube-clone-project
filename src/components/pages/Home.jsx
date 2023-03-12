37a9bde30507055a0dea8821ee2a5e69ee68ddcc
pjungjs <pjungjs@gmail.com>
2023-03-12 10:48:19 -0400

    added search bar on home.jsx file.

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,6 +1,34 @@
+import { useState } from "react";
 
 export default function Home() {
+  const [title, setTitle] = useState("");
+  const [searchToggle, setSearchToggle] = useState(false);
+
+  function updateSearch(event) {
+    const searchResult = event.target.value;
+    if (!searchResult) {
+      setSearchToggle(false);
+    }
+    setTitle(searchResult);
+  }
+ 
+  function handleSearch(event) {
+    event.preventDefault();
+    setSearchToggle(true);
+  }
+
   return (
-    <></>
+    <>
+      <form onSubmit={handleSearch}>
+        <input type="text" id="search" placeholder="Search..." value={title} onChange={updateSearch} />
+        <button  type="submit">Search</button>
+      </form>
+
+      {
+        !searchToggle
+        ? <p className="noSearch">No Search Result Yet! Please submit a search above!</p>
+        : "here goes the VideosList component"
+      }
+    </>
   );
 }

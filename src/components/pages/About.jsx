83dadf8d4cee582ba21dacbed56ff7131e28bcf3
Diegocarrero <diegocar2413@gmail.com>
2023-03-14 12:53:27 -0400

    Add content to About page

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,9 +1,44 @@
 
 export default function About() {
   return (
-    <div className="about">
-      <div className="Diego"></div>
-      <div className="Jinseok"></div>
+    <div>
+      <div className="about-section">
+        <h1>About Us</h1>
+        <p>We are developing Software Engineers honing our skills with React.js.</p>
+        <p>
+          This project serves to showcase our learning of React by creating a simplified clone of the YouTube website. 
+          This web application will allow you to watch videos based on any input you wish to search by using the YouTube API.
+        </p>
+      </div>
+      <h2 style='text-align:center'>Our Team</h2>
+      <div className="row">
+
+        <div className="column">
+          <div className="card">
+            <img src='' alt='Jinseok' style='width:100%'/>
+            <div className="container">
+              <h2>Jinseok Jung</h2>
+              <p className="title">Co-Creator</p>
+              <p>Text that describes Jinseok.</p>
+              <p>pjungjs@gmail.com</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="column">
+          <div className="card">
+            <img src="" alt="Diego" style='width:100%'/>
+            <div className="container">
+              <h2>Diego Carrero</h2>
+              <p className="title">Co-Creator</p>
+              <p></p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      
+      
     </div>
   );
 }

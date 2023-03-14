11afbc47884e8fe41a54e3a643328fb4b34d3345
Diegocarrero <diegocar2413@gmail.com>
2023-03-14 11:59:22 -0400

    fetches videos only

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -3,7 +3,7 @@ const URL = process.env.REACT_APP_BASE_URL;
 const KEY = process.env.REACT_APP_API_KEY;
 
 export async function getAllVideos(searchTitle, maxItems) {
-  const response = await fetch(`${URL}&q=${searchTitle}&maxResults=${maxItems}&key=${KEY}`);
+  const response = await fetch(`${URL}&type=video&q=${searchTitle}&maxResults=${maxItems}&key=${KEY}`);
   const result = await response.json();
   return result;
 }
